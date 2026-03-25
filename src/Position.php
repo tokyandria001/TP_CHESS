d623eda43671e4c81ea9b4807497cecd0d0bf9dc
tokyandria001 <toky.andrianjafy2001@gmail.com>
2026-03-25 10:07:05 +0100

    feat: Initialisation du TP, et mise en place de la Classe Position

---
+++ src/Position.php
@@ -0,0 +1,46 @@
+<?php
+
+class Position{
+    private int $row;
+    private int $column;
+
+    public function __construct(int $row, int $column){
+        if($row < 0 || $row > 7){
+            throw new \InvalidArgumentException("La ligne doit être entre 0 et 7.");
+        }
+
+        if($column < 0 || $column > 7){
+            throw new \InvalidArgumentException("La colonne doit être entre 0 et 7.");
+        }
+
+        $this->row = $row;
+        $this->column = $column;
+    }
+
+    public function getRow(): int {
+        return $this->row;
+    }
+
+    public function getColumn(): int {
+        return $this->column;
+    }
+
+    public function equals(Position $other): bool {
+        return $this->row === $other->getRow() && $this->column === $other->getColumn();
+    }
+
+    public function toKey(): string {
+        return $this->row . ':' . $this->column;
+    }
+
+    public static function fromKey(string $key): Position {
+        $parts = explode(':', $key);
+        if(count($parts) != 2){
+            throw new \InvalidArgumentException("Clé de position invalide !");
+        }
+        $row = (int)$parts[0];
+        $column = (int)$parts[1];
+
+        return new self($row, $column);
+    }
+}
